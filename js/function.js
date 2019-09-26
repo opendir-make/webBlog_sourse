2a854297831f0bc9cab0e829b15f9ea7be235788
Jackie1123 <1257868332@qq.com>
2019-09-26 17:07:27 +0800

    add return top function v2

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-var i=-1;
+			var i=-1;
 			function changeimg(){
 				var images=['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg'];
 				if(i<images.length-1){
